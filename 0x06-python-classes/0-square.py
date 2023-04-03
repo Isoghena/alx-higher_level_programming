237ca5b9e07a8a522c164cce937e31931216991e
Isoghena <estherewaia@yahoo.com>
2023-04-03 12:30:47 -0700

    squares

--- 0x06-python-classes/0-square.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-"""Defines a square."""
-
-class square:
-    """Empty."""
-    pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""Define a class."""
+
+class square:
+    """A square"""
+
+    def __init__(self,size):
+        """Initialize the square.
+        Args:
+            size (int): Size of square.
+        """
+        self._size = size
+
