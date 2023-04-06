2375d89896b6ca15e801073cb28796601808a395
Isoghena <estherewaia@yahoo.com>
2023-04-06 14:22:25 -0700

    task

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -6,17 +6,19 @@ Define a class Rectangle.
 
 class Rectangle:
     """Represents a rectangle"""
-    
-        def __init__(self, width=0, height=0):
+    def __init__(self, width=0, height=0):
+        """Intialize rectangle"""
              self.width = width
              self.height = height
 
         @property
         def width(self):
+            """getter for private inatance attribute"""
             return self.__width
 
         @width.setter
         def width(self, value):
+            """setter for private instance attribute"""
             if type(value) is not int:
                 raise TypeError("width must be an integer")
             if value < 0:
@@ -25,23 +27,14 @@ class Rectangle:
 
         @property
         def height(self):
+            """getter for private attribute instance"""
             return self.__height
 
         @height.setter
         def height(self, value):
+            """setter for private attribut instance"""
             if type(value) is not int:
                 raise TypeError("height must be an integer")
             if value < 0:
                 raise ValueError("height must be >= 0")
-            self.__height = value
-
-        def area(self):
-            """ calculate the area """
-            return self.__width * self.__height
-
-        def perimeter(self):
-            """ calculate the perimeter """
-            if self.__width is 0 or self.__height is 0:
-                return 0
-            return (self.__width * 2) + (self.__height * 2)
-                                                                                                        
+            self.__height = value                                                                             
