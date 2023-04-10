b7ba67155b2366b0b401d87c993ddea865a26f8d
Isoghena <estherewaia@yahoo.com>
2023-04-10 02:55:56 -0700

    task

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,24 +1,16 @@
 #!/usr/bin/python3
-    """ 
-    Adding the sum of two integers.
-    """
+""" 
+Adding the sum of two integers.
+"""
 
 
-    def add_integer(a, b=98):
-        """Return the sum of two int or two float.
-        Args:
-            a:first argument.Must be an integer else throw an error.
-            b:second argument.default int =98.
-        
-        Returns:sum of two integers or float.
-
-        Raises:Type error
-        """
-        if type (a) is int or type (a) is float:
-            if type (b) is int or type (b) is float:
-                return int(a+b)
+def add_integer(a, b=98):
+"""Return the sum of two integers."""
+    if type (a) is int or type (a) is float:
+        if type (b) is int or type (b) is float:
+            return int(a+b)
             else:
                 raise TypeError("b must be an integer or float")
-        else:
-            raise TyperError ("a must be an integer or float")
+    else:
+        raise TypeError("a must be an integer or float")
 
