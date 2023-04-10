9fc8f7b8ca3009c2303e82a9f58783c16881ed2b
Isoghena <estherewaia@yahoo.com>
2023-04-10 03:02:40 -0700

    task

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,16 +1,13 @@
 #!/usr/bin/python3
-""" 
-Adding the sum of two integers.
-"""
+"""This module provides a function that adds integers."""
 
 
 def add_integer(a, b=98):
-"""Return the sum of two integers."""
-    if type (a) is int or type (a) is float:
-        if type (b) is int or type (b) is float:
-            return int(a+b)
-            else:
-                raise TypeError("b must be an integer or float")
+    """Return the sum of two integers."""
+    if type(a) is int or type(a) is float:
+        if type(b) is int or type(b) is float:
+            return int(a + b)
+        else:
+            raise TypeError("b must be an integer")
     else:
-        raise TypeError("a must be an integer or float")
-
+        raise TypeError("a must be an integer")
