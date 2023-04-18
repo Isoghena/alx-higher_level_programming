781d9c534dd6a1bfdece842125546a76fa05d86d
Isoghena <estherewaia@yahoo.com>
2023-04-18 00:20:43 -0700

    add

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -5,7 +5,7 @@ This module provides a function that adds integers.
 """
 
 
-def add_integer(a, b):
+def add_integer(a, b=98):
     """Return the sum of two integers."""
     if type(a) is int or type(a) is float:
         if type(b) is int or type(b) is float:
